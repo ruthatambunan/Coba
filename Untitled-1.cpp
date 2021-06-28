336fb9a70ebb1559987cf289f58d497d9e85a6ab
Ruth Anggie Tambunan <ruthtambunan1@gmail.com>
2021-06-28 19:45:58 +0700

    Create Untitled-1.cpp

---
+++ Untitled-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, jumlah;
+    cout << "Masukkan angka pertama : "; cin >> a;
+    cout << "Masukkan angka kedua   : "; cin >> b;
+    cout << "Masukkan angka ketiga  : "; cin >> c;
+    jumlah = a+b+c;
+    cout << "Jumlah dari ketiga angka tersebut adalah : " << jumlah << endl;
+}
